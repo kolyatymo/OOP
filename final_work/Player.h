810992b28d9825a244f950f7eebc2b6354ada120
Kolya Tymoshchuk <kolya.tymo@gmail.com>
2026-06-15 20:58:23 +0300

    final

--- final_work/Player.h
+++ final_work/Player.h
@@ -1,4 +1,8 @@
-﻿//#pragma once
+﻿
+//class Player
+//{
+//};
+//#pragma once
 //#include <conio.h>
 //
 //enum KeyCode
@@ -14,28 +18,3 @@
 //{
 //private:
 //    int x, y;
-//
-//public:
-//    Player()
-//    {
-//        x = 10;
-//        y = 10;
-//    }
-//
-//    void move(int key)
-//    {
-//        if (key == UP) y--;
-//        else if (key == DOWN) y++;
-//        else if (key == LEFT) x--;
-//        else if (key == RIGHT) x++;
-//
-//        // межі карти
-//        if (x < 0) x = 0;
-//        if (x >= 20) x = 19;
-//        if (y < 0) y = 0;
-//        if (y >= 20) y = 19;
-//    }
-//
-//    int getX() { return x; }
-//    int getY() { return y; }
-//};
